17820600482a6b99fc669fca9f26d7e45e5f6097
remi-ui <r.m.thuss@hotmail.com>
2021-10-26 19:52:04 +0200

    added BERT and a small training sample of 1k facts

--- bot.py
+++ bot.py
@@ -1,7 +1,10 @@
 import discord
 import random
 
-TOKEN = 'ODk0ODkzNDM0MTMxODQxMDM1.YVwodQ.aRq8_dFxle6fdkc_TnmqTXqt_Zc'
+import transformers
+from transformers import pipeline
+
+TOKEN = 'ODk0ODkzNDM0MTMxODQxMDM1.YVwodQ.IQtlTUT-tPXWbX5LuWQegULbqWk'
 
 client = discord.Client()
 
@@ -22,11 +25,20 @@ async def on_message(message):
 	if user_message.lower() == 'i have a question':
 		await message.channel.send(f'Sure {username}, go ahead')
 		return
-	elif user_message.lower() == 'hello':
-		await message.channel.send(f'Hi {username}!')
+	else:
+		nlp = pipeline("question-answering", model = 'distilbert-base-cased-distilled-squad')
+		with open('train.txt', encoding="utf8") as file:
+			context = file.read()
+		await message.channel.send(nlp(question=user_message.lower(), context=context)['answer'])
 		return
-	elif user_message.lower() == 'what is the capital of france?':
-		await message.channel.send(f'Paris')
-		return
+
+	# elif user_message.lower() == 'hello':
+	# 	await message.channel.send(f'Hi {username}!')
+	# 	return
+	# elif user_message.lower() == 'who developed league of legends?':
+	# 	await message.channel.send(f'Riot games')
+	# elif user_message.lower() == 'how many people play fifa?':
+	# 	await message.channel.send(f'25 milion')
+	# 	return
 
 client.run(TOKEN)
