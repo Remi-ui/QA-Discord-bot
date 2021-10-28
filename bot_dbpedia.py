fb2a1810c69d032afd770d3dc46dbf140b54a69d
remi-ui <r.m.thuss@hotmail.com>
2021-10-28 11:20:09 +0200

    bot working with dbpedia now

---
+++ bot_dbpedia.py
@@ -0,0 +1,64 @@
+import discord
+import random
+import asyncio
+
+import transformers
+from transformers import pipeline
+
+import requests
+import json
+
+TOKEN = '#####'
+
+client = discord.Client()
+
+class MyClient(discord.Client):
+	@client.event
+	async def on_ready(self):
+		print('We have logged in as {0.user}'.format(client))
+
+	@client.event
+	async def on_message(self, message):
+		username = str(message.author).split('#')[0]
+		user_message = str(message.content)
+		channel = str(message.channel.name)
+		print(f'{username}: {user_message}({channel})')
+
+		if message.author.id == self.user.id:
+			return
+
+		if user_message.lower() == 'hi quan':
+			await message.channel.send(f'Hi {username}, what would you like to know something about?')
+			
+			try:
+				topic = await self.wait_for('message', timeout=10.0)
+			except asyncio.TimeoutError:
+				return await message.channel.send(f'Sorry I did not get that')
+
+			dash_topic = topic.content.replace(" ", "_")
+			u = "http://dbpedia.org/data/{}.json".format(dash_topic)
+			data = requests.get(u)
+			json_data = json.loads(data.content)
+
+			if not json_data:
+				return await message.channel.send(f'I am sorry, I do not know much about {topic.content}')
+
+			await message.channel.send(f'What would you like to know about {topic.content}?')
+
+			try:
+				question = await self.wait_for('message', timeout=15.0)
+			except asyncio.TimeoutError:
+				return await message.channel.send(f'Sorry I did not get that')
+
+			nlp = pipeline("question-answering", model = 'distilbert-base-cased-distilled-squad')
+
+			for j in json_data["http://dbpedia.org/resource/{}".format(dash_topic)]:
+				if(j == "http://dbpedia.org/ontology/abstract"):
+					context = [abstract['value'] for abstract in json_data["http://dbpedia.org/resource/{}".format(dash_topic)]["http://dbpedia.org/ontology/abstract"] if abstract['lang'] == 'en'][0]
+
+			await message.channel.send(nlp(question=question.content.lower(), context=context)['answer'])
+			return
+
+client = MyClient()
+
+client.run(TOKEN)
